d2e552cfa73cca3c8f916ee6709b3ff105375124
astrange90 <astrange90@hotmail.com>
2024-04-06 08:58:58 -0700

    fixed the get and post routes to match the fetch request

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,7 +2,7 @@ const notes = require('express').Router();
 const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
-notes.get('/', (req, res) => {
+notes.get('/api/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
